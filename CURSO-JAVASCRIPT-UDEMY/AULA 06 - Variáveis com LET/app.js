4b31b2de966f169cfa58f852290d81225e8fd85e
MarcosLenilson <janajesuslelemarcos@gmail.com>
2023-02-07 05:40:05 -0300

    AULA 06

---
+++ CURSO-JAVASCRIPT-UDEMY/AULA 06 - Variáveis com LET/app.js
@@ -0,0 +1,31 @@
+console.log('João nasceu em 1986.');
+console.log('Em 2000, João conheceu Maria.');
+console.log('João casou-se com Maria em 2012.');
+console.log('Em 2015, nasceu o primeiro filho de João.');
+console.log('O filho de João se chama Marcos ');
+
+let name = 'João'; // Let é a mesma coisa que Var
+
+console.log(name, 'nasceu em 1986.');
+console.log('Em 2000,', name, 'conheceu Maria.');
+console.log(name, 'casou-se com Maria em 2012.');
+console.log('Em 2015, nasceu o primeiro filho de', name, '.');
+console.log('O filho de', name, 'se chama Marcos. ');
+
+let name1;
+console.log(name1);  // Variável declarada mas não inicializada
+
+name1 = 'Marcos '
+console.log(name1);  // Variável declarada E inicializada
+
+
+
+// Sempre utilize LET para variaveis que possam ser alterados no futuro 
+// Não pode ter variaveis com o mesmo nome
+// Não podemos redeclarar variaveis com LET
+// Não podemos criar variáveis com palavras reservadas
+// Precisam ter nomes SIGNIFICATIVOS
+// O nome da variável não pode começar com numeros
+// Não pode ter espaços no nome
+// Sempre se utiliza o camelCase
+// São Case-sensitive
